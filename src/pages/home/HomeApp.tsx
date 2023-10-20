530c57119a80a5910d22193c39d816236a8b2a80
JangGusWjd <jjanghj3624@gmail.com>
2023-10-20 12:57:16 +0900

    Add HomeApp.tsx : home 화면 페이지 구현

---
+++ src/pages/home/HomeApp.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import HomeIntroduce from "../../components/home/HomeIntroduce";
+import HomeLogin from "../../components/home/HomeLogin";
+import HomeSearch from "../../components/home/HomeSearch";
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
+const HomeLeft = styled.div`
+  width: 40%;
+  height: 100vh;
+  max-width: 400px;
+  min-width: 350px;
+  background: #1b1c3a;
+`;
+
+const HomeRight = styled.div`
+  flex: 1;
+  overflow: auto;
+`;
+
+const HomeApp = () => {
+  return (
+    <HomeContainer>
+      <HomeLeft>
+        <HomeLogin />
+        <hr />
+        <HomeSearch />
+      </HomeLeft>
+      <HomeRight>
+        <HomeIntroduce />
+      </HomeRight>
+    </HomeContainer>
+  );
+};
+
+export default HomeApp;
