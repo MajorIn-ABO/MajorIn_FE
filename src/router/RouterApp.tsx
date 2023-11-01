e8e83880864a6bb245bbc515fe58456f812f3dcf
JangGusWjd <jjanghj3624@gmail.com>
2023-11-01 15:19:56 +0900

    Update RouterApp.tsx : 라우터 경로 변경

--- src/router/RouterApp.tsx
+++ src/router/RouterApp.tsx
@@ -1,13 +1,51 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+import styled from "styled-components";
 import HomeApp from "../pages/home/HomeApp";
 import MainApp from "../pages/main/MainApp";
+import StudyApp from "../pages/study/StudyApp";
+import CommunityApp from "../pages/community/CommunityApp";
+import TradeApp from "../pages/trade/TradeApp";
+import MyPageApp from "../pages/mypage/MyPageApp";
+import Header from "../components/navbar/Header";
+import Navbar from "../components/navbar/Navbar";
+
+const ContentContainer = styled.div`
+  display: flex;
+  width: 100%;
+  min-width: 1024px;
+  height: 90vh;
+  margin: 0 auto;
+  padding: 30px 20px;
+  background: #fafafa;
+  gap: 20px;
+`;
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Header />
+      <ContentContainer>
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
+      </ContentContainer>
+    </div>
+  );
+};
 
 const RouterApp = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomeApp />} />
-        <Route path="/main" element={<MainApp />} />
+        <Route path="/home" element={<HomeApp />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<MainApp />} />
+          <Route path="study" element={<StudyApp />} />
+          <Route path="community" element={<CommunityApp />} />
+          <Route path="trade" element={<TradeApp />} />
+          <Route path="mypage" element={<MyPageApp />} />
+        </Route>
       </Routes>
     </div>
   );
