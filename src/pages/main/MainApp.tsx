08403a4aa1710e9840007f2164460e37816495c1
JangGusWjd <jjanghj3624@gmail.com>
2023-11-06 21:49:16 +0900

    Update MainApp.tsx : 메인페이지에 필요한 컴포넌트 import 및 구현

--- src/pages/main/MainApp.tsx
+++ src/pages/main/MainApp.tsx
@@ -1,5 +1,33 @@
+import MainCommunity from "../../components/main/MainCommunity";
+import MainEmploy from "../../components/main/MainEmploy";
+import MainStudy from "../../components/main/MainStudy";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  width: 100%;
+  // background: grey;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  // grid-template-rows: 1fr 1.2fr;
+  row-gap: 40px;
+  column-gap: 20px;
+`;
+
+const MainCommunityContainer = styled.div`
+  grid-column: 1 / span 2;
+  grid-rows: 1 / span 1;
+`;
+
 const MainApp = () => {
-  return <div>MainPage</div>;
+  return (
+    <MainContainer>
+      <MainCommunityContainer>
+        <MainCommunity />
+      </MainCommunityContainer>
+      <MainStudy />
+      <MainEmploy />
+    </MainContainer>
+  );
 };
 
 export default MainApp;
