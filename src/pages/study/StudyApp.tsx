aaa5afe510be7caeebf4bc6096070fff8d5c9d43
JangGusWjd <jjanghj3624@gmail.com>
2023-11-08 17:57:52 +0900

    Update StudyApp.tsx : 스터디 페이지 ui 구현

--- src/pages/study/StudyApp.tsx
+++ src/pages/study/StudyApp.tsx
@@ -1,5 +1,37 @@
+import StudySearch from "../../components/study/StudySearch";
+import StudyList from "../../components/study/StudyList";
+import PopularStudy from "../../components/study/PopularStudy";
+import styled from "styled-components";
+
+const StudyContainer = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 30px;
+`;
+
+const StudyContent = styled.div`
+  width: 80%;
+`;
+
+// const PopularStudyContainer = styled.div`
+//   width: 20%;
+
+//   @media (max-width: 1370px) {
+//     display: none;
+// `;
+
 const StudyApp = () => {
-  return <div>StudyPage</div>;
+  return (
+    <StudyContainer>
+      <StudyContent>
+        <StudySearch />
+        <StudyList />
+      </StudyContent>
+      {/* <PopularStudyContainer> */}
+      <PopularStudy />
+      {/* </PopularStudyContainer> */}
+    </StudyContainer>
+  );
 };
 
 export default StudyApp;
