069129c114776d8b0e8a9d49c4eeeb358e023065
JangGusWjd <jjanghj3624@gmail.com>
2023-11-27 14:45:45 +0900

    Update CommunityApp.tsx : 커뮤니티 페이지 구성 컴포넌트 추가

--- src/pages/community/CommunityApp.tsx
+++ src/pages/community/CommunityApp.tsx
@@ -1,5 +1,21 @@
+import CommunityMain from "../../components/community/CommunityMain";
+import PopularCommunity from "../../components/community/PopularCommunity";
+import styled from "styled-components";
+
+const CommunityContainer = styled.div`
+  width: 90%;
+  min-height: 100vh;
+  display: flex;
+  gap: 30px;
+`;
+
 const CommunityApp = () => {
-  return <div>CommunityPage</div>;
+  return (
+    <CommunityContainer>
+      <CommunityMain />
+      <PopularCommunity />
+    </CommunityContainer>
+  );
 };
 
 export default CommunityApp;
