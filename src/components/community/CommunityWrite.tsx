04c1afcd04970b0ed9fa2f6a2b4b8f60c10f69d4
JangGusWjd <jjanghj3624@gmail.com>
2023-11-29 14:42:50 +0900

    Add CommunityWrite.tsx : 커뮤니티 글 작성 컴포넌트 ui 구현

---
+++ src/components/community/CommunityWrite.tsx
@@ -0,0 +1,44 @@
+import "../../styles/community/CommunityWrite.scss";
+
+const CommunityWrite: React.FC<{ onBackToMain: () => void }> = ({
+  onBackToMain,
+}) => {
+  return (
+    <div className="community-container">
+      <h1>✏️ 커뮤니티 글 작성</h1>
+      <p className="title-description">
+        커뮤니티 게시판에 질문할 내용, 공유하고 싶은 내용을 작성해주세요.
+      </p>
+      <form className="community-form">
+        <label>제목</label>
+        <input type="text" className="title" />
+        <label>내용</label>
+        <textarea className="content" />
+        <div>
+          <label>카테고리 선택</label>
+          <select>
+            <option value="전공질문">전공질문</option>
+            <option value="잡담/수다">잡담/수다</option>
+            <option value="인턴후기">인턴후기</option>
+            <option value="대외활동">대외활동</option>
+            <option value="우리학교는">우리학교는</option>
+          </select>
+        </div>
+        <div>
+          <label>사진 첨부</label>
+          <div className="filebox">
+            <input type="text" placeholder="사진 첨부" />
+            <label htmlFor="file">파일찾기</label>
+            <input type="file" id="file" />
+          </div>
+        </div>
+        <div className="button-zip">
+          <button onClick={onBackToMain}>취소</button>
+          <button onClick={onBackToMain}>등록</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CommunityWrite;
