3322447c8635c3d32ec8b7e26b6e864eb05c4084
JangGusWjd <jjanghj3624@gmail.com>
2024-03-18 16:27:01 +0900

    Design MainCommunity.tsx : 스크랩 아이콘 변경

--- src/components/main/MainCommunity.tsx
+++ src/components/main/MainCommunity.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as IconThree } from "../../assets/icon/number3.svg";
 import { ReactComponent as IconFour } from "../../assets/icon/number4.svg";
 import { ReactComponent as ChatIcon } from "../../assets/icon/chat-color.svg";
 import { ReactComponent as LikeIcon } from "../../assets/icon/like-color.svg";
-import { ReactComponent as StarIcon } from "../../assets/icon/star-color.svg";
+import { ReactComponent as ScrapIcon } from "../../assets/icon/scrap-color.svg";
 import "../../styles/main/MainCommunity.scss";
 import { useNavigate } from "react-router-dom";
 
@@ -93,11 +93,11 @@ const MainCommunity = () => {
             </div>
             <div className="content-box-bottom">
               <div className="content-numbers">
-                <ChatIcon stroke="#44B0FF" />
+                <ChatIcon stroke="#66BB6A" />
                 <p className="color-chat">{item.chat}</p>
                 <LikeIcon stroke="#FF8181" />
                 <p className="color-like">{item.like}</p>
-                <StarIcon />
+                <ScrapIcon />
                 <p className="color-scrap">{item.scrap}</p>
               </div>
               <button>자세히보기</button>
