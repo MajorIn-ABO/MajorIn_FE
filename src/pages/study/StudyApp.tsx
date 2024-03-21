640dd72ac1b02d741bc958cc4299c08a98a7d3fc
JangGusWjd <jjanghj3624@gmail.com>
2024-03-21 15:25:39 +0900

    Update StudyApp.tsx : 스터디 상세보기 페이지 라우터 연결 추가

--- src/pages/study/StudyApp.tsx
+++ src/pages/study/StudyApp.tsx
@@ -1,11 +1,15 @@
 import StudySearch from "../../components/study/StudySearch";
 import StudyList from "../../components/study/StudyList";
 import PopularStudy from "../../components/study/PopularStudy";
+import StudyDetail from "../../components/study/StudyDetail";
+import StudyWriter from "../../components/study/StudyWriter";
 import styled from "styled-components";
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 const StudyContainer = styled.div`
   width: 90%;
+  min-height: 100vh;
   display: flex;
   gap: 30px;
 `;
@@ -18,14 +22,32 @@ const StudyApp = () => {
   const [selectedFilter, setSelectedFilter] = useState("all");
   return (
     <StudyContainer>
-      <StudyContent>
-        <StudySearch
-          selectedFilter={selectedFilter}
-          onFilterChange={setSelectedFilter}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <StudyContent>
+                <StudySearch
+                  selectedFilter={selectedFilter}
+                  onFilterChange={setSelectedFilter}
+                />
+                <StudyList selectedFilter={selectedFilter} />
+              </StudyContent>
+              <PopularStudy />
+            </>
+          }
         />
-        <StudyList selectedFilter={selectedFilter} />
-      </StudyContent>
-      <PopularStudy />
+        <Route
+          path="/:studyId"
+          element={
+            <>
+              <StudyDetail />
+              <StudyWriter />
+            </>
+          }
+        />
+      </Routes>
     </StudyContainer>
   );
 };
