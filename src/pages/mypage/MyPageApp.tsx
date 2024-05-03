996c66b67c5ef7df43eec1dac590f7f151f837d3
JangGusWjd <jjanghj3624@gmail.com>
2024-05-03 18:57:14 +0900

    Update (MyPageApp.tsx) : 마이페이에 필요한 컴포넌트 불러오기

--- src/pages/mypage/MyPageApp.tsx
+++ src/pages/mypage/MyPageApp.tsx
@@ -1,5 +1,38 @@
+import MyPost from "../../components/mypage/MyPost";
+import MyTrade from "../../components/mypage/MyTrade";
+import MyInfo from "../../components/mypage/MyInfo";
+import styled from "styled-components";
+
+const MyPageContainer = styled.div`
+  width: 90%;
+  min-height: 100vh;
+  display: flex;
+  gap: 30px;
+`;
+
+const MyPostContent = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+const MyInfoContent = styled.div`
+  width: 30%;
+`;
+
 const MyPageApp = () => {
-  return <div>MyPage</div>;
+  return (
+    <MyPageContainer>
+      <MyPostContent>
+        <MyPost />
+        <MyTrade />
+      </MyPostContent>
+      <MyInfoContent>
+        <MyInfo />
+      </MyInfoContent>
+    </MyPageContainer>
+  );
 };
 
 export default MyPageApp;
