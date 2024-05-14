5a1ae709b540a4005f6a147bae7acd8b120dc279
JangGusWjd <jjanghj3624@gmail.com>
2024-05-14 13:15:33 +0900

    Add (recoilAtoms.tsx) : 회원가입 데이터 상태관리를 위한 recoil 파일 구현

---
+++ src/data/recoilAtoms.tsx
@@ -0,0 +1,19 @@
+import { atom } from "recoil";
+import { StudentData } from "../types/Types";
+
+export const studentDataState = atom<StudentData>({
+  key: "studentDataState", // 상태를 식별하기 위한 고유한 키입니다.
+  default: {
+    user_name: "장현정",
+    school_name: "단국대학교",
+    major_name: "경영학과",
+    student_id: 32203928,
+    major_id: 0,
+    home_id: "",
+    home_password: "",
+    home_password_check: "",
+    email: "",
+    phonenumber: "",
+    admission_date: 0,
+  },
+});
