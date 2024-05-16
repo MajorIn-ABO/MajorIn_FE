fbe8546e437a40363effc00781737f63e2735126
JangGusWjd <jjanghj3624@gmail.com>
2024-05-16 13:44:06 +0900

    Update : 로그인 api 연결

--- src/components/home/HomeLogin.tsx
+++ src/components/home/HomeLogin.tsx
@@ -1,8 +1,41 @@
 import "../../styles/home/HomeLogin.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { postData } from "../../api/postData";
+import { useAuth } from "../../hooks/useAuth";
 
 const HomeLogin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === "username") {
+      setUsername(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleLogin = async () => {
+    const loginData = {
+      username,
+      password,
+    };
+
+    const response = await postData("/login/", loginData);
+    // console.log("로그인 폼 전달: ", response);
+
+    if (response) {
+      const data = await response.token;
+      login(data.access, data.refresh);
+      alert("로그인에 성공하였습니다.");
+      navigate("/");
+    }
+  };
+
   const goSignUp = () => {
     navigate("/signup");
   };
@@ -12,9 +45,21 @@ const HomeLogin = () => {
         <img src={require("../../assets/img/appLogo.png")} alt="logo" />
       </div>
       <div className="login">
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="패스워드" />
-        <input type="button" value="로그인" />
+        <input
+          type="text"
+          placeholder="아이디"
+          name="username"
+          value={username}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          placeholder="패스워드"
+          name="password"
+          value={password}
+          onChange={handleInputChange}
+        />
+        <input type="button" value="로그인" onClick={handleLogin} />
       </div>
       <div className="login-bottom">
         <p>아이디 찾기</p>
--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,9 +1,42 @@
 import { ReactComponent as LogoIcon } from "../../assets/icon/logo2.svg";
 import "../../styles/login/LoginForm.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { postData } from "../../api/postData";
+import { useAuth } from "../../hooks/useAuth";
 
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === "username") {
+      setUsername(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleLogin = async () => {
+    const loginData = {
+      username,
+      password,
+    };
+
+    const response = await postData("/login/", loginData);
+    // console.log("로그인 폼 전달: ", response);
+
+    if (response) {
+      const data = await response.token;
+      login(data.access, data.refresh);
+      alert("로그인에 성공하였습니다.");
+      navigate("/");
+    }
+  };
+
   const goSignUp = () => {
     navigate("/signup");
   };
@@ -18,13 +51,25 @@ const LoginForm = () => {
         <h3>MajorIn</h3>
       </div>
       <form className="login-form">
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="패스워드" />
+        <input
+          type="text"
+          placeholder="아이디"
+          name="username"
+          value={username}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          placeholder="패스워드"
+          name="password"
+          value={password}
+          onChange={handleInputChange}
+        />
         <div>
           <input type="radio" id="login-keep" />
           <label htmlFor="login-keep">로그인 상태 유지</label>
         </div>
-        <input type="button" value="로그인" />
+        <input type="button" value="로그인" onClick={handleLogin} />
       </form>
       <div className="login-bottom">
         <p>아이디 찾기</p>
