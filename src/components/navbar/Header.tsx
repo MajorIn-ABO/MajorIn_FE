34302fdd9bff0f4d4dc342bd9e7d1994710bc13a
JangGusWjd <jjanghj3624@gmail.com>
2024-05-16 13:44:42 +0900

    Update : 로그인 여부에 따른 이벤트 적용

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -2,9 +2,12 @@ import "../../styles/navbar/Header.scss";
 import { ReactComponent as Logo } from "../../assets/icon/logo.svg";
 import { ReactComponent as SearchIcon } from "../../assets/icon/search.svg";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { auth, logout } = useAuth();
+
   const goHome = () => {
     navigate("/home");
   };
@@ -17,8 +20,11 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-container">
-        <div className="header-left">
-          <Logo onClick={goHome} />
+        <div
+          className="header-left"
+          style={{ cursor: auth.isLoggedIn ? "default" : "pointer" }}
+        >
+          <Logo onClick={auth.isLoggedIn ? undefined : goHome} />
           <div className="header-name">
             <p>IT</p>
             <p>MajorIn</p>
@@ -29,10 +35,16 @@ const Header = () => {
             <input type="text" placeholder="검색할 키워드를 입력하세요." />
             <SearchIcon />
           </div>
-          <div className="header-login">
-            <button onClick={goLogin}>로그인</button>
-            <button onClick={goSignUp}>회원가입</button>
-          </div>
+          {auth.isLoggedIn ? (
+            <div className="header-login">
+              <button onClick={logout}>로그아웃</button>
+            </div>
+          ) : (
+            <div className="header-login">
+              <button onClick={goLogin}>로그인</button>
+              <button onClick={goSignUp}>회원가입</button>
+            </div>
+          )}
         </div>
       </div>
     </div>
