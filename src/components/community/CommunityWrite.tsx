0ad6a80be31226df518a5d8e0c239af21a7c538f
JangGusWjd <jjanghj3624@gmail.com>
2024-05-17 16:17:13 +0900

    Update : 커뮤니티 글쓰기 api 연결

--- src/components/community/CommunityWrite.tsx
+++ src/components/community/CommunityWrite.tsx
@@ -1,8 +1,59 @@
 import "../../styles/community/CommunityWrite.scss";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { postTokenData } from "../../api/postData";
+import { CommunityWriteData } from "../../types/Types";
 
 const CommunityWrite: React.FC<{ onBackToMain: () => void }> = ({
   onBackToMain,
 }) => {
+  const [writeData, setWriteData] = useState<CommunityWriteData>({
+    title: "",
+    contents: "",
+    category_name: "",
+    imgfile: new File([], ""),
+  });
+  const [fileName, setFileName] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setWriteData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setWriteData((prevData) => ({
+        ...prevData,
+        imgfile: file,
+      }));
+      setFileName(file.name);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", writeData.title);
+    formData.append("contents", writeData.contents);
+    formData.append("category_name", writeData.category_name);
+    formData.append("imgfile", writeData.imgfile);
+
+    const responseData = await postTokenData("/boards/posts/create/", formData);
+    if (responseData) {
+      alert("글 등록이 완료되었습니다.");
+      navigate("/community");
+    }
+  };
+
   return (
     <div className="community-container">
       <h1>✏️ 커뮤니티 글 작성</h1>
@@ -11,15 +62,30 @@ const CommunityWrite: React.FC<{ onBackToMain: () => void }> = ({
       </p>
       <form className="community-form">
         <label>제목</label>
-        <input type="text" className="title" />
+        <input
+          type="text"
+          className="title"
+          name="title"
+          value={writeData.title}
+          onChange={handleChange}
+        />
         <label>내용</label>
-        <textarea className="content" />
+        <textarea
+          className="content"
+          name="contents"
+          value={writeData.contents}
+          onChange={handleChange}
+        />
         <div>
           <label>카테고리 선택</label>
-          <select>
-            <option value="전공질문">학과질문</option>
+          <select
+            name="category_name"
+            value={writeData.category_name}
+            onChange={handleChange}
+          >
+            <option value="학과질문">학과질문</option>
             <option value="잡담/수다">잡담/수다</option>
-            <option value="인턴후기">인턴리뷰</option>
+            <option value="인턴리뷰">인턴리뷰</option>
             <option value="대외활동">대외활동</option>
             <option value="우리학교는">우리학교는</option>
           </select>
@@ -27,14 +93,19 @@ const CommunityWrite: React.FC<{ onBackToMain: () => void }> = ({
         <div>
           <label>사진 첨부</label>
           <div className="filebox">
-            <input type="text" placeholder="사진 첨부" />
+            <input
+              type="text"
+              placeholder="사진 첨부"
+              value={fileName}
+              readOnly
+            />
             <label htmlFor="file">파일찾기</label>
-            <input type="file" id="file" />
+            <input type="file" id="file" onChange={handleFileChange} />
           </div>
         </div>
         <div className="button-zip">
           <button onClick={onBackToMain}>취소</button>
-          <button onClick={onBackToMain}>등록</button>
+          <button onClick={handleSubmit}>등록</button>
         </div>
       </form>
     </div>
