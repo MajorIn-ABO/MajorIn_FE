4b1a59f4da57445b4e923d96f757e73ff7c88d65
JangGusWjd <jjanghj3624@gmail.com>
2024-05-18 01:10:51 +0900

    Update (PostData.tsx) : 텍스트 데이터, 판매완료 여부 post용 메소드 추가

--- src/api/postData.tsx
+++ src/api/postData.tsx
@@ -5,14 +5,14 @@ const API_BASE_URL = "http://3.35.123.253/api";
 export const postData = async (endpoint: string, data: any) => {
   try {
     const response = await axios.post(`${API_BASE_URL}${endpoint}`, data);
-    console.log("서버로부터의 응답:", response.data);
+    // console.log("서버로부터의 응답:", response.data);
     return response.data;
   } catch (error) {
     console.log(error);
   }
 };
 
-// 토큰을 포함한 상태에서 post 요청 메소드
+// 토큰을 포함한 상태에서 이미지 포함한 post 요청 메소드
 export const postTokenData = async (endpoint: string, formData: FormData) => {
   const storedAuth = localStorage.getItem("auth");
   const auth = storedAuth ? JSON.parse(storedAuth) : null;
@@ -32,6 +32,40 @@ export const postTokenData = async (endpoint: string, formData: FormData) => {
       },
     });
 
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postTextData = async (endpoint: string, data: any) => {
+  const storedAuth = localStorage.getItem("auth");
+  const auth = storedAuth ? JSON.parse(storedAuth) : null;
+  const accessToken = auth ? auth.access_token : null;
+  const userId = auth ? auth.user_id : null;
+  const postData = { ...data, user_id: userId };
+  try {
+    const response = await axios.post(`${API_BASE_URL}${endpoint}`, postData, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postSold = async (endpoint: string) => {
+  const storedAuth = localStorage.getItem("auth");
+  const auth = storedAuth ? JSON.parse(storedAuth) : null;
+  const accessToken = auth ? auth.access_token : null;
+  try {
+    const response = await axios.post(`${API_BASE_URL}${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     return response.data;
   } catch (error) {
     console.log(error);
