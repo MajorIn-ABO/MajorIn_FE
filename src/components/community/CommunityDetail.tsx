ba23121ccfa8a667b4b2f43e12c1defffa00e093
JangGusWjd <jjanghj3624@gmail.com>
2024-06-06 20:55:23 +0900

    Add : 커뮤니티, 스터디, 중고거래 댓글 api 연결

--- src/components/community/CommunityDetail.tsx
+++ src/components/community/CommunityDetail.tsx
@@ -1,46 +1,79 @@
 import { ReactComponent as ChatIcon } from "../../assets/icon/chat-color.svg";
 import { ReactComponent as LikeIcon } from "../../assets/icon/like-color.svg";
 import { ReactComponent as ScrapIcon } from "../../assets/icon/scrap-color.svg";
-// import { ReactComponent as GoodIcon } from "../../assets/icon/good.svg";
-// import { ReactComponent as BadIcon } from "../../assets/icon/bad.svg";
-// import { ReactComponent as UserIcon } from "../../assets/icon/user.svg";
-// import { ReactComponent as ReplyIcon } from "../../assets/icon/reply.svg";
+import { ReactComponent as GoodIcon } from "../../assets/icon/good.svg";
+import { ReactComponent as BadIcon } from "../../assets/icon/bad.svg";
+import { ReactComponent as UserIcon } from "../../assets/icon/user.svg";
+import { ReactComponent as ReplyIcon } from "../../assets/icon/reply.svg";
 import { ReactComponent as SendIcon } from "../../assets/icon/send.svg";
 import "../../styles/community/CommunityDetail.scss";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { CommunityData } from "../../types/Types";
+import { CommunityData, CommentData } from "../../types/Types";
 import { fetchData } from "../../api/fetchData";
+import { postTextData } from "../../api/postData";
 
 const CommunityDetail = () => {
   const navigate = useNavigate();
   const { contentId } = useParams();
   const parsedContentId = contentId ? parseInt(contentId) : undefined;
+  const [comment, setComment] = useState("");
 
   const [selectedData, setSelectedData] = useState<CommunityData>();
+  const [commentData, setCommentData] = useState<CommentData[]>([]);
+
+  const fetchCommunityData = async () => {
+    const data = await fetchData(`/boards/posts/${parsedContentId}/`);
+    const comments = await fetchData(
+      `/boards/posts/comments-by-postid/${parsedContentId}/`
+    );
+    setSelectedData(data);
+    if (comments) {
+      setCommentData(comments.sort((a: any, b: any) => b.id - a.id));
+    }
+  };
 
   useEffect(() => {
-    const fetchCommunityData = async () => {
-      const data = await fetchData(`/boards/posts/${parsedContentId}/`);
-      setSelectedData(data);
-    };
-
-    fetchCommunityData();
+    if (parsedContentId) {
+      fetchCommunityData();
+    }
   }, [parsedContentId]);
 
-  // 각 댓글의 좋아요/싫어요 상태를 관리하는 객체
-  const [commentReactions, setCommentReactions] = useState<{
-    [key: number]: {
-      likeColor: string;
-      dislikeColor: string;
-      currentColor: string;
-      like: number;
-    };
-  }>({});
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(event.target.value);
+  };
 
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const postData = {
+      post_id: parsedContentId,
+      contents: comment,
+    };
+
+    // console.log(postData);
+    const response = await postTextData(
+      "/boards/posts/comments/create/",
+      postData
+    );
+    if (response) {
+      await fetchCommunityData();
+      setComment("");
+    }
+  };
   if (!selectedData) {
     return <div>해당 컨텐츠를 찾을 수 없습니다.</div>;
   }
+
+  // 각 댓글의 좋아요/싫어요 상태를 관리하는 객체
+  // const [commentReactions, setCommentReactions] = useState<{
+  //   [key: number]: {
+  //     likeColor: string;
+  //     dislikeColor: string;
+  //     currentColor: string;
+  //     like: number;
+  //   };
+  // }>({});
+
   // const commentData = selectedData?.comments || [];
 
   // 좋아요 클릭 시 색상 변경 함수
@@ -125,26 +158,26 @@ const CommunityDetail = () => {
           </div>
         </div>
       </div>
-      {/* {commentData?.map((comment, index) => {
-        const { likeColor, dislikeColor, currentColor, like } =
-          commentReactions[comment.commentId] || {
-            likeColor: "#BBBBBB",
-            dislikeColor: "#BBBBBB",
-            currentColor: "#777777",
-            like: comment.like,
-          };
+      {commentData?.map((comment, index) => {
+        // const { likeColor, dislikeColor, currentColor, like } =
+        //   commentReactions[comment.commentId] || {
+        //     likeColor: "#BBBBBB",
+        //     dislikeColor: "#BBBBBB",
+        //     currentColor: "#777777",
+        //     like: comment.like,
+        //   };
         return (
           <div key={index} className="community-comment-container">
             <div className="comment-like">
               <GoodIcon
-                onClick={() => handleLikeClick(comment.commentId)}
-                fill={likeColor}
+                // onClick={() => handleLikeClick(comment.commentId)}
+                // fill={likeColor}
                 style={{ cursor: "pointer" }}
               />
-              <p style={{ color: currentColor }}>{like}</p>
+              {/* <p style={{ color: currentColor }}>{like}</p> */}
               <BadIcon
-                onClick={() => handleDislikeClick(comment.commentId)}
-                fill={dislikeColor}
+                // onClick={() => handleDislikeClick(comment.commentId)}
+                // fill={dislikeColor}
                 style={{ cursor: "pointer" }}
               />
             </div>
@@ -153,8 +186,16 @@ const CommunityDetail = () => {
                 <div className="top-left">
                   <UserIcon width="24" height="24" />
                   <div>
-                    <p>{comment.writer}</p>
-                    <p>{comment.date}</p>
+                    <p>
+                      {comment.school_name} {comment.major_name}{" "}
+                      {String(comment.admission_date).slice(-2)}학번{" "}
+                    </p>
+                    <p>
+                      {new Date(comment.comment_date).toLocaleString("ko-KR", {
+                        dateStyle: "medium",
+                        timeStyle: "short",
+                      })}
+                    </p>
                   </div>
                 </div>
                 <span>
@@ -162,17 +203,22 @@ const CommunityDetail = () => {
                   답글 달기
                 </span>
               </div>
-              <p className="bottom">{comment.content}</p>
+              <p className="bottom">{comment.contents}</p>
             </div>
           </div>
         );
-      })} */}
+      })}
       <div className="community-comment-write">
         <button onClick={goCommunity}>목록으로</button>
-        <div>
-          <input type="text" placeholder="댓글을 입력하세요." />
-          <SendIcon stroke="#ffffff" />
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="댓글을 입력하세요."
+            value={comment}
+            onChange={handleInputChange}
+          />
+          <SendIcon stroke="#ffffff" type="submit" />
+        </form>
       </div>
     </div>
   );
