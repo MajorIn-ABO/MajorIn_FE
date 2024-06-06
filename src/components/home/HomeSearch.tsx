3f8d8d307fb8ee816af63fc74fac6f421537e241
JangGusWjd <jjanghj3624@gmail.com>
2024-06-06 20:59:14 +0900

    Chore : 가상 데이터 값 변경

--- src/components/home/HomeSearch.tsx
+++ src/components/home/HomeSearch.tsx
@@ -20,6 +20,26 @@ const HomeSearch = () => {
           컴퓨터공학, 소프트웨어, 정보통신, 정보보호, IT융합, 멀티미디어
         </span>
       </div>
+      <div className="home-major-list" onClick={goMain}>
+        <p>경영경제</p>
+        <span>경영, 회계, 마케팅, 금융, 경제</span>
+      </div>
+      <div className="home-major-list" onClick={goMain}>
+        <p>인문사회과학</p>
+        <span>국어국문, 영어영문, 사회학, 심리학, 정치외교, 사학,</span>
+      </div>
+      <div className="home-major-list" onClick={goMain}>
+        <p>공과</p>
+        <span>전자공학, 기계공학, 화학공학, 건축공학, 신소재공학</span>
+      </div>
+      <div className="home-major-list" onClick={goMain}>
+        <p>자연과학</p>
+        <span>물리학, 화학, 생물학, 지구과학, 수학, 통계학</span>
+      </div>
+      <div className="home-major-list" onClick={goMain}>
+        <p>예술대학</p>
+        <span>미술, 패션디자인, 음악, 연극영화, 무용</span>
+      </div>
     </div>
   );
 };
