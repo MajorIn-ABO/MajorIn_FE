52b8c48aa83b8e49f9f914f99ead38070a7a7a9a
JangGusWjd <jjanghj3624@gmail.com>
2024-06-17 15:59:44 +0900

    Fix : navbar, header 경로 수정

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -87,54 +87,58 @@ const Navbar = () => {
       <div className="menu-list">
         <ul>
           <StyledLink
-            to="/"
-            className={location.pathname === "/" ? "active" : ""}
+            to="/main"
+            className={location.pathname === "/main" ? "active" : ""}
           >
             <HomeIcon
-              fill={location.pathname === "/" ? "#1b1c3a" : "#9b9b9b"}
+              fill={location.pathname === "/main" ? "#1b1c3a" : "#9b9b9b"}
             />
             <p>홈</p>
           </StyledLink>
           <StyledLink
-            to="/study"
-            onClick={(e) => handleClick(e, "/study")}
-            className={location.pathname === "/study" ? "active" : ""}
+            to="/main/study"
+            onClick={(e) => handleClick(e, "/main/study")}
+            className={location.pathname === "/main/study" ? "active" : ""}
           >
             <StudyIcon
-              fill={location.pathname === "/study" ? "#1b1c3a" : "#9b9b9b"}
-              stroke={location.pathname === "/study" ? "#1b1c3a" : "#9b9b9b"}
+              fill={location.pathname === "/main/study" ? "#1b1c3a" : "#9b9b9b"}
+              stroke={
+                location.pathname === "/main/study" ? "#1b1c3a" : "#9b9b9b"
+              }
             />
             <p>스터디</p>
           </StyledLink>
           <StyledLink
-            to="/community"
-            onClick={(e) => handleClick(e, "/community")}
-            className={location.pathname === "/community" ? "active" : ""}
+            to="/main/community"
+            onClick={(e) => handleClick(e, "/main/community")}
+            className={location.pathname === "/main/community" ? "active" : ""}
           >
             <CommunityIcon
               stroke={
-                location.pathname === "/community" ? "#1b1c3a" : "#9b9b9b"
+                location.pathname === "/main/community" ? "#1b1c3a" : "#9b9b9b"
               }
             />
             <p>커뮤니티</p>
           </StyledLink>
           <StyledLink
-            to="/trade"
-            onClick={(e) => handleClick(e, "/trade")}
-            className={location.pathname === "/trade" ? "active" : ""}
+            to="/main/trade"
+            onClick={(e) => handleClick(e, "/main/trade")}
+            className={location.pathname === "/main/trade" ? "active" : ""}
           >
             <BookIcon
-              fill={location.pathname === "/trade" ? "#1b1c3a" : "#9b9b9b"}
+              fill={location.pathname === "/main/trade" ? "#1b1c3a" : "#9b9b9b"}
             />
             <p>중고거래</p>
           </StyledLink>
           <StyledLink
-            to="/mypage"
-            onClick={(e) => handleClick(e, "/mypage")}
-            className={location.pathname === "/mypage" ? "active" : ""}
+            to="/main/mypage"
+            onClick={(e) => handleClick(e, "/main/mypage")}
+            className={location.pathname === "/main/mypage" ? "active" : ""}
           >
             <MypageIcon
-              fill={location.pathname === "/mypage" ? "#1b1c3a" : "#9b9b9b"}
+              fill={
+                location.pathname === "/main/mypage" ? "#1b1c3a" : "#9b9b9b"
+              }
             />
             <p>마이페이지</p>
           </StyledLink>
